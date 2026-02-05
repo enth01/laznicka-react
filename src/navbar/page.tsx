8d9235daf40d1a93e89ee909230c5142ad727476
enth01 <kmec7896@gmail.com>
2026-02-05 09:50:58 +0100

    mimimi

--- src/navbar/page.tsx
+++ src/navbar/page.tsx
@@ -3,15 +3,17 @@ export default function Navbar() {
         <div className="absolute top-0 left-0 navbar bg-base-100 shadow-sm">
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
-                <li><a>Home</a></li>
-                <li><a>Products</a></li>
-                <li><a>Profile</a></li>
-                <li><a>Cart</a></li>
-                <li><a>Dashboard</a></li>
-                <li><a href="/login">Login</a></li>
-                <li><a href="/register">Register</a></li>
-                <li><a>Logout</a></li>
+                    <li><a>Home</a></li>
+                    <li><a>Products</a></li>
+                    <li><a>Profile</a></li>
+                    <li><a>Cart</a></li>
+                    <li><a>Dashboard</a></li>
+                    <li><a href="/login">Login</a></li>
+                    <li><a href="/register">Register</a></li>
                 </ul>
+            </div>
+            <div className="navbar-end">
+                <button className="absolute right-0 mr-3">Logout</button>
             </div>
         </div>
     )
